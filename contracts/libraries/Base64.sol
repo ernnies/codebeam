7c0bc39ad06b086a2bb33c96bc8d3ea129fe4fd9
ermnies <ernestnnamdie@gmail.com>
2024-11-21 13:53:51 +0100

    base64

--- contracts/libraries/Base64.sol
+++ contracts/libraries/Base64.sol
@@ -50,6 +50,6 @@ library Base64 {
                     out,
                     and(mload(add(tablePtr, and(shr(6, input), 0x3F))), 0xFF)
                 )
-                out := shl(8,
+                out := shl(
         return strisult);
     }
