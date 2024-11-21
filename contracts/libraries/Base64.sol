3765d0444ce10b9cf76c65907205064726acf66a
ermnies <ernestnnamdie@gmail.com>
2024-11-21 13:47:33 +0100

    base64

--- contracts/libraries/Base64.sol
+++ contracts/libraries/Base64.sol
@@ -57,6 +57,6 @@ library Base64 {
             case 1 {
                 mstore(sub(resultPtr, 2), shl(240, 0x3d3d))
             }
-            case 2 {
+            case 2
         return strisult);
     }
